f55c62755914c72f1759bc480489119dbb7cfbac
void48730-cloud <void48730@gmail.com>
2026-03-08 01:04:27 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,16 @@
-let cart =
-JSON.parse(localStorage.getItem("cart")) || [];
+// Sepet sayısı göstergesi
+function updateCartCount(){
+  const count = JSON.parse(localStorage.getItem("cart"))?.length || 0;
+  document.getElementById("cartCount").textContent = count;
+}
 
-const div =
-document.getElementById("cart");
+// Sayfa yüklendiğinde güncelle
+updateCartCount();
 
-cart.forEach(item => {
-
-  div.innerHTML +=
-    item.name + " - " + item.price + "₺<br>";
-
+// Her ürün sepete eklendiğinde güncelle
+document.querySelectorAll(".product").forEach(product=>{
+  const buyBtn = product.querySelector(".buyBtn");
+  buyBtn.addEventListener("click",()=>{
+    setTimeout(updateCartCount, 50); // ürün eklendikten sonra count güncelle
+  });
 });
