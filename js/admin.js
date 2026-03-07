57072ef96e3577798125d6821b926ca361652619
void48730-cloud <void48730@gmail.com>
2026-03-08 00:40:05 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,46 @@
+const password = "1234";
 
+function login(){
+
+const p =
+document.getElementById("pass").value;
+
+if(p === password){
+
+document.getElementById(
+"panel"
+).style.display="block";
+
+}
+
+}
+
+function addKey(){
+
+let product =
+document.getElementById("product").value;
+
+let key =
+document.getElementById("key").value;
+
+let keys =
+JSON.parse(
+localStorage.getItem("keys")
+) || {};
+
+if(!keys[product]){
+
+keys[product] = [];
+
+}
+
+keys[product].push(key);
+
+localStorage.setItem(
+"keys",
+JSON.stringify(keys)
+);
+
+alert("eklendi");
+
+}
